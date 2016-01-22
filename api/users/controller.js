82c444a6f1e23cafcd452258a20ed6b8e938168b
Alon Robinson <ahrobins3@gmail.com>
2016-01-22 01:01:33 -0800

    add database queries to route functions

--- api/users/controller.js
+++ api/users/controller.js
@@ -1,9 +1,19 @@
 
 exports.show = function(req,res){
-
+  User.find({}, function(err, users){
+    if(err){
+      console.log('error finding users')
+    }
+    res.json(users);
+  })
 }
 
 exports.showOne = function(req,res){
+  User.findById(req.body.id, function(err, user){
+    if(!err){
+      res.json(user);
+    }
+  })
 
 }
 exports.create = function(req,res){
